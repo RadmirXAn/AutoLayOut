7da54155de8add870f7fee83708e85228cc45113
RadMirXAn <rrradmirrr@mail.ru>
2020-12-01 12:57:43 +0500

    ios

---
+++ AutoLayaout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayaout
+//
+//  Created by Радмир Юмагужин on 01.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
